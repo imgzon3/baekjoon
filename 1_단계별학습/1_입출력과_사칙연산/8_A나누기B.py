7313da9f0904f06842a916baa91b7ecdaa5418c8
imgzon3 <imgzon3@naver.com>
2021-01-10 00:08:41 +0900

    baekjoon

---
+++ 1_단계별학습/1_입출력과_사칙연산/8_A나누기B.py
@@ -0,0 +1,5 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+# print(round((a / b), 2))
+print(a/b)
---
+++ 1_단계별학습/1_입출력과_사칙연산/9_사칙연산.py
@@ -0,0 +1,9 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+# a, b = map(int, input().split)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)1
