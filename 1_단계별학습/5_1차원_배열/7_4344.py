935a46f5663d9d47476b332eef6490a40167e884
imgzon3 <imgzon3@naver.com>
2021-01-12 19:39:11 +0900

    list complete

--- 1_단계별학습/5_1차원_배열/7_4344.py
+++ 1_단계별학습/5_1차원_배열/7_4344.py
@@ -1,1 +1,15 @@
 num = int(input())
+for i in range(num):
+    n = map(int, input().split())
+    n = list(n)
+    avg = 0
+    for k in range(1, len(n)):
+        avg += n[k]
+    avg = avg // n[0]
+    
+    count = 0
+    for k in range(1, len(n)):
+        if n[k] > avg:
+            count += 1
+    
+    print('{:.3f}%'.format(count/n[0]*100))
