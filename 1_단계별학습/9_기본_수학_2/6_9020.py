9f54f0559bb4cfaf0be5fd192a4e8ef45c15207e
imgzon3 <imgzon3@naver.com>
2021-01-31 22:30:35 +0900

    9020 change

--- 1_단계별학습/9_기본_수학_2/6_9020.py
+++ 1_단계별학습/9_기본_수학_2/6_9020.py
@@ -1,9 +1,9 @@
 num = int(input())
 
-limit = 10000 # n이 123456까지 입력되므로 그보다 2배 더 큰 값까지 계산
+limit = 10000 # 10000까지의 소수 구하기
 
 num = [True for i in range(limit + 1)] # 소수 판단 bool list
-num[0] = False
+num[0] = False # 0과 1은 소수가 아니다
 num[1] = False
 for i in range(2, int(limit**0.5) + 1): # 남은 수 중 아직 처리하지 않은 i
     if num[i] == True:
@@ -15,4 +15,5 @@ for i in range(2, int(limit**0.5) + 1): # 남은 수 중 아직 처리하지 않
 result = [i for i in range(2, limit+1) if num[i]] # limit 까지의 소수 리스트
 
 for _ in num:
+    n = int(input()) # 주어지는 짝수
     
