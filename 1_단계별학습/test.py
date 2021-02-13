31ce7f7fcaf497d624977836e35e47dd5ef9825a
imgzon3 <imgzon3@naver.com>
2021-02-13 23:58:46 +0900

    test

--- 1_단계별학습/test.py
+++ 1_단계별학습/test.py
@@ -1,26 +1,33 @@
 n = int(input())
 
-tmp = 0
-tmp_2 = 0
+tmp = 0 # tmp
+tmp_2 = 0 # 6관련된 것이면, 몇번 째 수인지
+result = 0 # 앞에 나오는 수
 status = 0 # 0이면 일반, 10이면 6, 100이면 66, 1000이면 666
-for _ in range(n):
+for i in range(n):
     if tmp == n:
+        result = i
         break
     elif tmp > n:
-        tmp_2 = tmp - status
-        
+        result = i
+        tmp_2 = status - tmp + n
+        break
     else:
-        if tmp%1000 == 666:
+        print(tmp)
+        if i%1000 == 666:
             tmp += 1000
             status = 1000
-        elif tmp%100 == 66:
+        elif i%100 == 66:
             tmp += 100
             status = 100
-        elif tmp%10 == 6:
+        elif i%10 == 6:
+            tmp += 10
+            status = 10
+        elif i == 6:
             tmp += 10
             status = 10
         else:
             status = 0
             tmp += 1
 
-print(f'tmp:{tmp}, tmp_2:{tmp_2}')
+print(f'result:{result}, tmp_2:{tmp_2}')
