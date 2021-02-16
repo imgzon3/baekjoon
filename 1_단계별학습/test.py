6d1c0699d7d20a1b5dc9b968a076ecdb6bb57246
imgzon3 <imgzon3@naver.com>
2021-02-16 23:08:56 +0900

    test

--- 1_단계별학습/test.py
+++ 1_단계별학습/test.py
@@ -1,10 +1,3 @@
 n = int(input())
-res = 0
-six_n = 666
-while True:
-    if '666' in str(six_n):
-        res += 1
-    if res == n:
-        print(six_n)
-        break
-    six_n += 1
+for _ in range(n):
+    print(n)
