bf1d8066b72f3740e8c115480a5065eaf4f9cc6d
imgzon3 <imgzon3@naver.com>
2021-03-02 18:49:46 +0900

    add def count_sort()

--- 1_단계별학습/12_정렬/3_10989.py
+++ 1_단계별학습/12_정렬/3_10989.py
@@ -1,3 +1,15 @@
 '''
 문제 팁: 수의 범위가 작다면 카운팅 정렬 사용해 빠르게 정렬
 '''
+import sys
+
+def count_sort(arr: list, size: int, max: int):
+    count = [0 for _ in range(max+1)]
+    
+    for i in arr:
+        count[i] += 1
+    
+    for idx, i in enumerate(count):
+        if i!=0:
+            for k in range(i):
+                sys.stdout.write(str(idx)+'\n')
