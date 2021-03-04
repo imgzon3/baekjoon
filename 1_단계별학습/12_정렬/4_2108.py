af5e5d4d588a5f86f6337d11e3d64167d2f33704
imgzon3 <imgzon3@naver.com>
2021-03-04 20:49:27 +0900

    change 2108.py

--- 1_단계별학습/12_정렬/4_2108.py
+++ 1_단계별학습/12_정렬/4_2108.py
@@ -48,3 +48,32 @@
 
 # if __name__ == '__main__':
 #     main()
+import sys
+
+n = int(input())
+num = [int(sys.stdin.readline()) for _ in range(n)]
+
+# 산술평균
+mean = round(sum(num)/len(num))
+print(mean)
+
+# 중앙값
+sort_num = sorted(num)
+print(sort_num[len(num)//2])
+
+# 최빈값
+count = [0]*8001
+for i in num:
+    count[i+4000] += 1
+res = []
+for idx, i in enumerate(count):
+    if i==max(count):
+        res.append(idx-4000)
+if len(res)==1:
+    print(res[0])
+else:
+    print(sorted(res)[1])
+
+# 범위
+range = max(num) - min(num)
+print(range)
