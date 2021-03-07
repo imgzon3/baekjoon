4bca5e701d0f982bb7b2d8a4d748c0feaa398590
imgzon3 <imgzon3@naver.com>
2021-03-07 15:35:11 +0900

    change 15649.py

--- 1_단계별학습/13_백트래킹/1_15649.py
+++ 1_단계별학습/13_백트래킹/1_15649.py
@@ -1,11 +1,5 @@
 '''
 N, M에 따라 수열 작성
 무조건적으로 for문 늘리기에는 한계
+백트래킹이란, DFS 기반으로 불필요한 경우를 배제, 원하는 해답에 도달하는 탐색이다.
 '''
-import sys
-
-n, m = map(int, input().split())
-
-for i in range(1, n+1):
-    if m==1:
-        sys.stdout.write(str(i)+'\n')
