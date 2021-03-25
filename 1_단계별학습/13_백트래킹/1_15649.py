f77afd1c09405c48f5a4c8fd9bc0e2a441f86b80
imgzon3 <imgzon3@naver.com>
2021-03-26 00:23:51 +0900

    주석 추가

--- 1_단계별학습/13_백트래킹/1_15649.py
+++ 1_단계별학습/13_백트래킹/1_15649.py
@@ -3,7 +3,7 @@ N, M에 따라 수열 작성
 무조건적으로 for문 늘리기에는 한계
 백트래킹이란, DFS 기반으로 불필요한 경우를 배제, 원하는 해답에 도달하는 탐색이다.
 '''
-# python 내부함수 itertools의 permutations 활용할 시 중복하지 않는 수열을 찾을 수 있다고 함
+# python 내부함수 itertools의 permutations 활용할 시 중복하지 않는 수열을 찾을 수 있다고 함(이래서 내장 함수 꼭 확인)
 from itertools import permutations
 N, M = map(int, input().split())
 P = permutations(range(1, N+1), M)  # iter(tuple)
