a5a7630329b1017891c8ac7a92d1de53b49684e4
imgzon3 <imgzon3@naver.com>
2021-06-23 06:01:00 +0900

    test

--- 1_백준/1_단계별학습/test.py
+++ 1_백준/1_단계별학습/test.py
@@ -1,2 +1,14 @@
-for i, j in range(3), range(3):
-    print(i+' '+j)
+def solution(n):
+    num = [True for i in range(n+1)] # 0번째 index는 버린다
+    num[1] = False # 1은 소수가 아님
+    for i in range(1, int(n**0.5)+1):
+        if i!=1:
+            k=2
+            while i*k <= n:
+                num[i*k]=False
+                k+=1
+    result = [i for i in range(2, n+1) if num[i]]
+    return len(result)
+
+if __name__=="__main__":
+    print(solution(10))
