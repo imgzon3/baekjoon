be34e171bfe882566239fa93d1b6d0c7838665a3
imgzon3 <imgzon3@naver.com>
2021-06-27 00:29:46 +0900

    test

--- 1_백준/1_단계별학습/test.py
+++ 1_백준/1_단계별학습/test.py
@@ -1,14 +1,7 @@
-def solution(n):
-    num = [True for i in range(n+1)] # 0번째 index는 버린다
-    num[1] = False # 1은 소수가 아님
-    for i in range(1, int(n**0.5)+1):
-        if i!=1:
-            k=2
-            while i*k <= n:
-                num[i*k]=False
-                k+=1
-    result = [i for i in range(2, n+1) if num[i]]
-    return len(result)
-
-if __name__=="__main__":
-    print(solution(10))
+a = dict()
+a['1번'] = 1/8
+a['2번'] = 3/7
+a['3번'] = 2/4
+a['4번'] = 1/2
+a['5번'] = 0/1
+print(a)
